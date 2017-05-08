0be374b79d8bc1faa07b6bf97948ef7ae399769b
이성용 <sy.lee@dna.uno>
2017-05-08 15:59:25 +0900

    가격 모델 생성 및 쿼리 테스트

--- incatest/models.py
+++ incatest/models.py
@@ -6,13 +6,13 @@ from django.utils import timezone
 
 
 # Create your models here.
-class incaResults(models.Model):
-	wdate = models.CharField(max_length=10,  blank=True, null=True)
+class incaoutcomes(models.Model):
+	wdate = models.DateField(blank=True, null=True)
 	itemcode = models.CharField(max_length=20,  blank=True, null=True)
 	itemname = models.CharField(max_length=100,  blank=True, null=True)
-	DNA_score = models.DecimalField(max_digits=22, decimal_places=12, blank=True, null=True)
-	DNA_index = models.DecimalField(max_digits=22, decimal_places=12, blank=True, null=True)
-	probability = models.DecimalField(max_digits=22, decimal_places=12, blank=True, null=True)
+	DNA_score = models.DecimalField(max_digits=30, decimal_places=17, blank=True, null=True)
+	DNA_index = models.DecimalField(max_digits=30, decimal_places=17, blank=True, null=True)
+	probability = models.DecimalField(max_digits=30, decimal_places=17, blank=True, null=True)
 	MAX_rate = models.IntegerField(default=0)
 	MIN_rate = models.IntegerField(default=0)
 
@@ -27,6 +27,30 @@ class incaResults(models.Model):
 
 	def __str__(self):
 		return self.itemname.encode('utf-8')
+
+
+class incaprices(models.Model):
+	itemcode = models.CharField(max_length=20,  blank=True, null=True)
+	wdate = models.DateField(blank=True, null=True)
+	open = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+	high = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+	low = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+	close = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+	trading_volume = models.IntegerField(default=0)
+	trading_value = models.IntegerField(default=0)
+
+	class Meta:
+		db_table = 'stock_data'
+
+	def publish(self):
+		self.created_date = timezone.now()
+		
+	def change(self):
+		self.updated_date = timezone.now()
+
+	def __str__(self):
+		return self.itemcode.encode('utf-8')
+
 
 
 
--- incatest/views.py
+++ incatest/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from .models import incaResults
+from .models import incaoutcomes, incaprices
 
 import csv
 import json
@@ -15,8 +15,30 @@ from datetime import timedelta
 # Create your views here.
 def index(requesst):
 	cnt = 0
-	incaRes = incaResults.objects.all()
-	for incaRe in incaRes:
-		print incaRe.itemcode
+	# outcomes = incaoutcomes.objects.all()
+	# for outcome in outcomes:
+	# 	if cnt < 10:
+	# 		print " -------------------------- "
+	# 		print outcome.wdate
+	# 		print outcome.itemcode
+	# 		print outcome.DNA_score
+	# 		print outcome.DNA_index
+	# 		print outcome.probability
+	# 		print outcome.MAX_rate
+	# 		print outcome.MIN_rate
+	# 		print " ---------------------------- "
+
+	prices = incaprices.objects.filter(itemcode='KLVL01V2101', wdate__gte='2015-04-20', wdate__lte='2015-04-30')
+	for price in prices:
+		print " -------------------------- "
+		print price.itemcode
+		print price.wdate
+		# print price.open
+		# print price.high
+		# print price.low
+		print price.close
+		print price.trading_volume
+		print price.trading_value
+		print " ---------------------------- "
 		cnt = cnt + 1
 	return HttpResponse(cnt)
