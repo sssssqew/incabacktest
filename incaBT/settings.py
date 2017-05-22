515e9619cc8ebfba85c4d4e8b921ee4d26af78c0
sylee <sy.lee@dna.uno>
2017-05-22 22:20:56 +0900

    Update settings.py

--- incaBT/settings.py
+++ incaBT/settings.py
@@ -83,10 +83,10 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'KRX',
-        'USER': 'Stephanie',
-        'PASSWORD': 'ge1!ex2@le3#nu4$de5%',
-        'HOST': '1.214.33.234',
+        'NAME': 'exampleDB',
+        'USER': 'exampleUser',
+        'PASSWORD': 'examplePassword',
+        'HOST': 'exampleHost',
         'OPTIONS': {
             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
         },
